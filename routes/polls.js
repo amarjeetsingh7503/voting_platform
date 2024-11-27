7ffbd5ad41039e9aea1efe33c326df27409c9d21
PriyanshuPS <priyanshupratap@gmail.com>
2024-11-27 12:19:11 +0530

    login signup

--- routes/polls.js
+++ routes/polls.js
@@ -1,20 +1,90 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 const Poll = require("../models/poll");
+const User = require("../models/user");
 const router = express.Router();
 
+// Session Middleware
+router.use(
+  session({
+    secret: "6a9d89b2c3f15eb5c4f5d13bc7e6f29147a1babc32defaad1c12c2345f67890p",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+// Middleware to Check Authentication
+function isAuthenticated(req, res, next) {
+  if (req.session.userId) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
 // Home Page - List Polls
-router.get("/", async (req, res) => {
+router.get("/", isAuthenticated, async (req, res) => {
   const polls = await Poll.find();
-  res.render("index", { polls });
+  res.render("index",{ polls, session: req.session });
 });
 
+// Login Page
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Handle Login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    req.session.userId = user._id;
+    res.redirect("/");
+  } else {
+    // Pass error to the EJS view
+    res.render("login", { error: "Invalid username or password" });
+  }
+});
+// Signup Page
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// Handle Signup
+router.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  const existingUser = await User.findOne({ username });
+
+  if (existingUser) {
+    // Pass error to the EJS view if the username already exists
+    return res.render("signup", { error: "Username already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.redirect("/login");
+  } catch (err) {
+    res.render("signup", { error: "An error occurred during signup" });
+  }
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
+
+
 // New Poll Form
-router.get("/polls/new", (req, res) => {
+router.get("/polls/new", isAuthenticated, (req, res) => {
   res.render("new_poll");
 });
 
 // Create New Poll
-router.post("/polls/new", async (req, res) => {
+router.post("/polls/new", isAuthenticated, async (req, res) => {
   const { question, options } = req.body;
 
   const pollOptions = options.map((option) => ({
